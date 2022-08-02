be7a6ae5705dc20f90a3638fb2aa88e463e06ae4
AkshithaRajavel <2407akshi@gmail.com>
2022-08-02 13:25:28 +0530

    changes

--- codes/samples.cpp
+++ codes/samples.cpp
@@ -1,17 +1,7 @@
 #include<iostream>
 using namespace std;
-class Test{
-    public :
-    Test(int a,int b){
-
-    }
-};
 int main(){
-    Test t(100,200);
-    int arr[4]={1,2,3,4};
-    int ind;
-    cout<<"Index Limit : ";
-    cin>>ind;
-    for(int i=0;i<=ind;i++)
-    cout<< arr[i]<<" ";
+string s = "computer science";
+	char c[]="computer science";
+	cout<< sizeof(c)<<" c style" <<"\n"<< sizeof(s)<<" string object" ;
 }
