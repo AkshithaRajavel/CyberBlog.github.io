35613d0566c9b07ccc4a5ad22832a1f45a5d2171
AkshithaRajavel <2407akshi@gmail.com>
2022-08-02 13:43:41 +0530

    changes

--- codes/samples.cpp
+++ codes/samples.cpp
@@ -2,6 +2,5 @@
 using namespace std;
 int main(){
 string s = "computer science";
-	char c[]="computer science";
-	cout<< sizeof(c)<<" c style" <<"\n"<< sizeof(s)<<" string object" ;
+cout<<s.length();
 }
