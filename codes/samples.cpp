f08bf1a9f451140fea27515d613d85151438811b
AkshithaRajavel <2407akshi@gmail.com>
2022-08-02 14:06:22 +0530

    changes

--- codes/samples.cpp
+++ codes/samples.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 int main(){
 string s = "computer science";
-cout<<s.length();
+cout<<s.length()<<endl;//actual length
+int *ptr = (int*)&s;//pointer to string object in heap
+*((int*)*ptr-6) = 8;//changing length field to 8
+cout<<s;
 }
