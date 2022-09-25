b0881934e6a8ffebce08f0443b7d2962e0bf9202
AkshithaRajavel <2407akshi@gmail.com>
2022-09-25 23:21:37 +0530

    latest

--- codes/samples.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-string s = "computer science";
-cout<<s.length()<<endl;//actual length
-int *ptr = (int*)&s;//pointer to string object in heap
-*((int*)*ptr-6) = 8;//changing length field to 8
-cout<<s;
-}
